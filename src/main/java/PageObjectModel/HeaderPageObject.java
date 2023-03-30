4e90eae29e1cf36d3ef2c56a7549ecb92cd7101a
0812Vaishu <Vaishnaviyadav2050@gmail.com>
2023-03-30 16:01:37 +0530

    Final Project

--- src/main/java/PageObjectModel/HeaderPageObject.java
+++ src/main/java/PageObjectModel/HeaderPageObject.java
@@ -63,46 +63,4 @@ public class HeaderPageObject {
 			return driver.findElement(checkout);
 		}
 
-/*//Currency--
-	private By Currency = By.xpath("//button[@class='btn btn-link dropdown-toggle']");
-
-// TelePhone--
-	private By Telephone=By.xpath("//i[@class='fa fa-phone']");
-	
-    private By TelePhoneNumber =By.xpath("(//span[@class='hidden-xs hidden-sm hidden-md'])[2]");
-// myAccont--
-	private By MyAccount =By.xpath("(//span[@class='hidden-xs hidden-sm hidden-md'])[3]");
-	
-// wishList--
-	private By Wishlist =By.xpath("(//span[@class='hidden-xs hidden-sm hidden-md'])[4]");
-// ShoppingCart---
-	private By ShoppingCart =By.xpath("(//span[@class='hidden-xs hidden-sm hidden-md'])[5]");
-	
-// CheckOut--
-	private By checkOut =By.xpath("(//span[@class='hidden-xs hidden-sm hidden-md'])[6]");
-	
-	public HeaderPageObject(WebDriver driver2) {
-		this.driver = driver2;
-	}
-	public WebElement VerifyCurrency() {
-		return driver.findElement(Currency);
-	}
-	public WebElement ClickonTelephone() {
-		return driver.findElement(Telephone);
-	}
-	public WebElement verifyTelePhone() {
-		return driver.findElement(TelePhoneNumber);
-	}
-	public WebElement verifyMyAccount() {
-		return driver.findElement(MyAccount);
-	}
-	public WebElement verifyWishList() {
-		return driver.findElement(Wishlist);
-	}
-	public WebElement verifyShoppingCart() {
-		return driver.findElement(ShoppingCart);
-	}
-	public WebElement verifyCheckOut() {
-		return driver.findElement(checkOut);
-	}*/
 }
